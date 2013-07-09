151db2296833ca1bb0cf02145268e790451d1a14
Eivind <thoromdir@gmail.com>
2013-07-09 12:58:42 +0400

    speed improvements

--- tmaskcleaner/tmaskcleaner.cpp
+++ tmaskcleaner/tmaskcleaner.cpp
@@ -128,15 +128,13 @@ public:
     TMaskCleaner(PClip child, int length, int thresh, IScriptEnvironment*);
     PVideoFrame __stdcall GetFrame(int n, IScriptEnvironment* env);
 
-    ~TMaskCleaner() {
-        if(row!=nullptr) delete [] row;
-    }
+    ~TMaskCleaner() {}
 private:
     unsigned int m_length;
     unsigned int m_thresh;
-    int* row;
     DynamicBuffer<int> buffer;
     DynamicBuffer<BYTE> mask;
+    DynamicBuffer<Coordinates> coords;
     int m_w;
     int size;
 
@@ -147,9 +145,9 @@ TMaskCleaner::TMaskCleaner(PClip child, int length, int thresh, IScriptEnvironme
     GenericVideoFilter(child),
     m_length(length),
     m_thresh(thresh),
-    row(nullptr),
     buffer(length),
-    mask(child->GetVideoInfo().height * child->GetVideoInfo().width)
+    mask(child->GetVideoInfo().height * child->GetVideoInfo().width * 2),
+    coords(child->GetVideoInfo().height * child->GetVideoInfo().width)
 {
     if (!child->GetVideoInfo().IsYV12()) {
         env->ThrowError("Only YV12 and YV24 is supported!");
@@ -157,12 +155,13 @@ TMaskCleaner::TMaskCleaner(PClip child, int length, int thresh, IScriptEnvironme
     if (length <= 0 || thresh <= 0) {
         env->ThrowError("Invalid arguments!");
     }
-    m_w = child->GetVideoInfo().width;
-    int h= child->GetVideoInfo().height;
-    row = new int[h];
-    for(int i=0,v=0;i<h;i++,v+=m_w){
-        row[i]=v;
+    int CPUInfo[4]; //eax, ebx, ecx, edx
+    __cpuid(CPUInfo, 1);
+
+    if (!(CPUInfo[2] & 0x00000200)) {
+        env->ThrowError("Sorry, SSSE3 is required");
     }
+    m_w = child->GetVideoInfo().width;
 }
 
 PVideoFrame TMaskCleaner::GetFrame(int n, IScriptEnvironment* env) {
@@ -176,11 +175,12 @@ PVideoFrame TMaskCleaner::GetFrame(int n, IScriptEnvironment* env) {
 void TMaskCleaner::ClearMask(BYTE *dst, const BYTE *src, int w, int h, int src_pitch, int dst_pitch) {
     ArrayAccessor<int> buffer_accessor = buffer.GetBuffer();
     ArrayAccessor<BYTE> mask_accessor = mask.GetBuffer();
+    ArrayAccessor<Coordinates> coords_accessor = coords.GetBuffer();
     int* buf = buffer_accessor.ptr;
     BYTE* m = mask_accessor.ptr;
-    memset(m,1,h*w);
-    std::vector<Coordinates> coordinates;
-    int b;
+    Coordinates* coordinates = coords_accessor.ptr;
+    memset(m,1,h*src_pitch);
+    int b,cs;
     Coordinates current;
     for(int y = 0; y < h; ++y) {
         for(int x = 0; x < w; ++x) {
@@ -194,11 +194,10 @@ void TMaskCleaner::ClearMask(BYTE *dst, const BYTE *src, int w, int h, int src_p
             }
             buf[0]=pos;
             b=1;
-            coordinates.clear();
-            coordinates.emplace_back(x,y);
-            while(!coordinates.empty()){
-                current = coordinates.back();
-                coordinates.pop_back();
+            coordinates[0] = Coordinates(x,y);
+            cs = 1;
+            while(cs>0){
+                current = coordinates[--cs];
                 int x_min = current.first  == 0 ? 0 : current.first - 1;
                 int x_max = current.first  == w - 1 ? w : current.first + 2;
                 int y_min = current.second == 0 ? 0 : current.second - 1;
@@ -209,7 +208,7 @@ void TMaskCleaner::ClearMask(BYTE *dst, const BYTE *src, int w, int h, int src_p
                         if (m[pos]==1){
                             m[pos]=0;
                             if(src[pos]<=m_thresh){
-                                coordinates.emplace_back(i,j);
+                                coordinates[cs++] = Coordinates(i,j);
                                 if(b<m_length){
                                     buf[b++] = pos;
                                 } else {
@@ -229,12 +228,31 @@ void TMaskCleaner::ClearMask(BYTE *dst, const BYTE *src, int w, int h, int src_p
         }
     }
 
-    unsigned int* inp = (unsigned int*)src;
-    unsigned int* m = (unsigned int*)m;
-    unsigned int* res = (unsigned int*)dst;
-    int cnt = (w*h) / sizeof(unsigned int);
-    for (int i = 0 ; i < cnt ; i++ ){
-        res[i] = inp[i] & m[i];
+    int m16 = w / 16;
+    int mw = m16*16;
+    int sov = src_pitch - w;
+    int dov = dst_pitch - w;
+    for(int y = 0,sp =0 ,dp =0; y < h; y++){
+        for(int x=0; x < m16; x++){
+            __m128i a = _mm_loadu_si128(reinterpret_cast<const __m128i*>(src+sp));
+            __m128i b = _mm_loadu_si128(reinterpret_cast<const __m128i*>(m+sp));
+            _mm_storeu_si128(reinterpret_cast<__m128i*>(dst+dp), _mm_and_si128(a,b));
+            sp+=16;
+            dp+=16;
+        }
+        sp+=sov;
+        dp+=dov;
+    }
+    if(w>mw){
+        for(int y=0,sp=0,dp=0;y<h;y++){
+            sp += mw;
+            dp += mw;
+            for(int x=mw;x<w;x++,sp++,dp++){
+                dst[dp] = src[sp] & m[sp];
+            }
+            sp+= sov;
+            dp+= dov;
+        }
     }
 }
 
